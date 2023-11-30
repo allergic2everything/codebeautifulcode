0864c00a101cb3982c29ce04ede6e8f5b660de81
allergic2everything <142352433+allergic2everything@users.noreply.github.com>
2023-11-30 10:18:26 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(122, 255, 100);
+  arc(50, 50, 80, 80, 0, PI + QUARTER_PI);
+
+}
+
+//key press key release val of input do whatever 
