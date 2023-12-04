cbb1e07b652b0966a177c24d25724c23f60fb58a
allergic2everything <142352433+allergic2everything@users.noreply.github.com>
2023-12-04 10:25:11 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
   background(122, 255, 100);
-  arc(50, 50, 80, 80, 0, PI + QUARTER_PI);
+  getUserInput(); 
 
 }
 
@@ -13,7 +13,13 @@ function getUserInput() {
   var userInput = document.getElementById("userInput").value;
 
   // Do something with the user input (e.g., display it)
-  alert("User input: " + userInput);
+  if (userInput == "hi")  
+  {
+    arc(50, 50, 80, 80, 0, PI + QUARTER_PI);
+  }
+
+     
+   
 }
 
 //key press key release val of input do whatever 
